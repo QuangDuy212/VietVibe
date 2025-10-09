c4592f6caadc53e6b53aa51f2c034fc5ccd3237d
DaoQuangTung <daoquangtung3@gmail.com>
2025-10-09 20:37:47 +0700

    feat(backend): crud-lesson-and-not-relationship

---
+++ VietVibe/src/main/java/com/example/VietVibe/entity/Lesson.java
@@ -0,0 +1,55 @@
+package com.example.VietVibe.entity;
+
+import java.time.Instant;
+import com.example.VietVibe.util.SecurityUtil;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity(name = "lessons")
+@JsonPropertyOrder(alphabetic = true)
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JsonProperty("_id")
+    String id;
+
+    @Column(length = 512)
+    String refreshToken;
+
+    @Column(name = "lessontitle", unique = true, length = 255, nullable = false)
+    String lessontitle;
+    String videourl;
+    String description;
+
+    Instant createdAt;
+    Instant updatedAt;
+    String createdBy;
+    String updatedBy;
+
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        this.updatedAt = Instant.now();
+    }
+
+}
