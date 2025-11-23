2faf8b65a20fe469a39109769ce62850257c3fab
doduclong204 <doduclong2004gg@gmail.com>
2025-11-23 08:23:42 +0700

    frontend crud-point

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,9 +1,7 @@
 import { IAccount, IBackendRes, IGetAccount, IUser } from '@/types/common.type';
 import axios from './axios-customize';
 
-
 //MODULE AUTH
-
 export const callFetchAccount = () => {
     return axios.get<IBackendRes<IGetAccount>>('/api/v1/auth/account')
 }
@@ -23,4 +21,23 @@ export const callRefreshToken = () => {
 export const callLogout = () => {
     return axios.post<IBackendRes<string>>('/api/v1/auth/logout')
 }
+
+//MODULE CRUD POINT 
+export const callGetAllPoints = (page = 0, size = 10, sort?: string) => {
+    const oneIndexedPage = Math.max(1, page + 1);
+    return axios.get<any>('/api/v1/points', { params: { page: oneIndexedPage, size, sort } });
+};
+
+export const callUpdatePoint = (pointId: number, data: any) => {
+    return axios.put<IBackendRes<any>>(`/api/v1/points/${pointId}`, data);
+};
+
+export const callDeletePoint = (pointId: number) => {
+    return axios.delete<IBackendRes<any>>(`/api/v1/points/${pointId}`);
+};
+
+export const callSearchPoints = (data: any, page = 0, size = 10, sort?: string) => {
+    const oneIndexedPage = Math.max(1, page + 1);
+    return axios.post<any>('/api/v1/points/search', data, { params: { page: oneIndexedPage, size, sort } });
+};
 
