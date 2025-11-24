91a6e7b9b978f0afedec254b50251cc06ada47e8
DaoQuangTung <daoquangtung3@gmail.com>
2025-11-24 19:51:35 +0700

    feat: finished-frontend-crud-lessons

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,5 +1,6 @@
-import { IAccount, IBackendRes, IGetAccount, IUser } from '@/types/common.type';
+import { IAccount, IBackendRes, IGetAccount, IUser, ILesson } from '@/types/common.type';
 import axios from './axios-customize';
+import { all } from 'axios';
 
 
 //MODULE AUTH
@@ -8,7 +9,7 @@ export const callFetchAccount = () => {
     return axios.get<IBackendRes<IGetAccount>>('/api/v1/auth/account')
 }
 
-export const callRegister = (data: {username: string , name?: string, password: string, address?: string,role?: string }) => {
+export const callRegister = (data: { username: string, name?: string, password: string, address?: string, role?: string }) => {
     return axios.post<IBackendRes<IUser>>('/api/v1/auth/register', data)
 }
 
@@ -24,3 +25,29 @@ export const callLogout = () => {
     return axios.post<IBackendRes<string>>('/api/v1/auth/logout')
 }
 
+//MODULE LESSONS
+const PREFIX_API = "api/v1/lessons";
+
+export const callFetchLessons = () => {
+    return axios.get<IBackendRes<ILesson[]>>(`/${PREFIX_API}/all`);
+}
+
+export const callCreateLesson = (lesson: {
+    lessontitle: string;
+    videourl: string;
+    description: string;
+}) => {
+    return axios.post<IBackendRes<ILesson>>(`/${PREFIX_API}`, lesson);
+}
+
+export const callUpdateLesson = (id: string, lesson: {
+    lessontitle: string;
+    videourl: string;
+    description: string;
+}) => {
+    return axios.put<IBackendRes<ILesson>>(`/${PREFIX_API}/${id}`, lesson);
+}
+
+export const callDeleteLesson = (id: string) => {
+    return axios.delete<IBackendRes<any>>(`/${PREFIX_API}/${id}`);
+}
